2f2f9031aeab2bf9df34cf772b10dffc9726d076
sifat <bsse1221@iit.du.ac.bd>
2025-01-26 21:10:18 +0600

    refactor common portion

--- codecapture_backend/api/views.py
+++ codecapture_backend/api/views.py
@@ -8,33 +8,37 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from api.preprocessing import extract_images, extract_unique_images
 
+
+def preprocessing(request):
+    if not os.path.exists(settings.IMAGES_DIR): os.makedirs(settings.IMAGES_DIR)
+    if os.path.exists(settings.IMAGES_DIR): 
+        shutil.rmtree(settings.IMAGES_DIR)
+        os.makedirs(settings.IMAGES_DIR)
+    if not os.path.exists(settings.VIDEOS_DIR): os.makedirs(settings.VIDEOS_DIR)
+    if os.path.exists(settings.VIDEOS_DIR): 
+        shutil.rmtree(settings.VIDEOS_DIR)
+        os.makedirs(settings.VIDEOS_DIR)
+    if not request.FILES.getlist('videos'): return JsonResponse({"error": "No files uploaded"}, status=400)
+
+    video_urls = []
+    for file in request.FILES.getlist('videos'):
+        file_path = os.path.join(settings.VIDEOS_DIR, file.name)
+        with open(file_path, 'wb+') as destination:
+            for chunk in file.chunks(): destination.write(chunk)
+        video_urls.append(file_path)
+
+    print("Note Generation Started")
+    extract_images(settings.VIDEOS_DIR, settings.IMAGES_DIR)
+    extract_unique_images(settings.IMAGES_DIR)
+
+
 @csrf_exempt
 def generate_notes(request):
     if request.method == "POST":
-        if not os.path.exists(settings.IMAGES_DIR): os.makedirs(settings.IMAGES_DIR)
-        if os.path.exists(settings.IMAGES_DIR): 
-            shutil.rmtree(settings.IMAGES_DIR)
-            os.makedirs(settings.IMAGES_DIR)
-        if not os.path.exists(settings.VIDEOS_DIR): os.makedirs(settings.VIDEOS_DIR)
-        if os.path.exists(settings.VIDEOS_DIR): 
-            shutil.rmtree(settings.VIDEOS_DIR)
-            os.makedirs(settings.VIDEOS_DIR)
-        if not request.FILES.getlist('videos'): return JsonResponse({"error": "No files uploaded"}, status=400)
-
-        video_urls = []
-        for file in request.FILES.getlist('videos'):
-            file_path = os.path.join(settings.VIDEOS_DIR, file.name)
-            with open(file_path, 'wb+') as destination:
-                for chunk in file.chunks(): destination.write(chunk)
-            video_urls.append(file_path)
-
-        print("Note Generation Started")
-        extract_images(settings.VIDEOS_DIR, settings.IMAGES_DIR)
-        extract_unique_images(settings.IMAGES_DIR)
-
-        zip_file_path = os.path.join(settings.MEDIA_ROOT, 'images.zip')
+        preprocessing(request)
+        zip_file_path = os.path.join(settings.BASE_DIR, 'images.zip')
         with zipfile.ZipFile(zip_file_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
-            for root, dirs, files in os.walk(settings.IMAGES_DIR):
+            for root,_, files in os.walk(settings.IMAGES_DIR):
                 for file in files:
                     zipf.write(os.path.join(root, file), os.path.relpath(os.path.join(root, file), settings.IMAGES_DIR))
 
@@ -45,3 +49,6 @@ def generate_notes(request):
 
     return JsonResponse({"error": "Invalid request method"}, status=400)
 
+@csrf_exempt
+def transcribe_video(request):
+    pass
