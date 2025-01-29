6b07c8249f811aee41f61fdb39da102a3cba9ef7
sifat <bsse1221@iit.du.ac.bd>
2025-01-29 17:21:28 +0600

    added transcribe and summarize video again

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import generate_notes, generate_notes_again , transcribe_video,summarize_video,extract_source_code,extract_workflow,generate_all,check_api
+from .views import generate_notes, generate_notes_again , transcribe_video,summarize_video,extract_source_code,extract_workflow,generate_all,check_api, transcribe_video_again
 urlpatterns = [
     path('generate_notes/', generate_notes),
     path('transcribe_video/', transcribe_video),
@@ -9,8 +9,8 @@ urlpatterns = [
     path('generate_all/', generate_all),
     path('check_api/', check_api),
     path('generate_notes/generate_again', generate_notes_again),
-    path('transcribe_video/generate_again', check_api),
-    path('summarize_video/generate_again', check_api),    
+    path('transcribe_video/generate_again', transcribe_video),
+    path('summarize_video/generate_again', summarize_video),    
     path('extract_source_code/generate_again', check_api),
     path('extract_workflow/generate_again', check_api),
     path('generate_all/generate_again', check_api),
